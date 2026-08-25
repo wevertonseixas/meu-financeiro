6e1f8fd4e5042b6a2a18c2315602f70fb4f97e35
Weverton Nascimento Seixas <166784783+wevertonseixas@users.noreply.github.com>
2026-08-24 21:36:01 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,80 +1,41 @@
-const CACHE_NAME = "meu-financeiro-v1";
+const CACHE_NAME = "meu-financeiro-v2";
 
-const ARQUIVOS = [
-    "./",
-    "./index.html",
-    "./manifest.json"
+const FILES_TO_CACHE = [
+  "/meu-financeiro/",
+  "/meu-financeiro/index.html",
+  "/meu-financeiro/manifest.json",
+  "/meu-financeiro/icon-192.png",
+  "/meu-financeiro/icon-512.png"
 ];
 
-
-// Instalação
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 
-    event.waitUntil(
-
-        caches.open(CACHE_NAME)
-            .then(cache => {
-
-                return cache.addAll(ARQUIVOS);
-
-            })
-
-    );
-
-    self.skipWaiting();
-
+  self.skipWaiting();
 });
 
-
-// Ativação
 self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
 
-    event.waitUntil(
-
-        caches.keys()
-            .then(chaves => {
-
-                return Promise.all(
-
-                    chaves
-                        .filter(
-                            chave =>
-                                chave !== CACHE_NAME
-                        )
-                        .map(
-                            chave =>
-                                caches.delete(chave)
-                        )
-
-                );
-
-            })
-
-    );
-
-    self.clients.claim();
-
+  self.clients.claim();
 });
 
-
-// Funcionamento offline
 self.addEventListener("fetch", event => {
-
-    event.respondWith(
-
-        caches.match(event.request)
-            .then(resposta => {
-
-                if (resposta) {
-
-                    return resposta;
-
-                }
-
-                return fetch(event.request);
-
-            })
-
-    );
-
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
 });
